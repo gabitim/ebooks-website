2a696e988b957b1142ba5fc1240976fa6451ebd2
Timofti Gabriel <gabitim470@gmail.com>
2022-01-07 22:55:44 +0200

    react typescript setup, and openapi generator

---
+++ src/components/main-page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const MainPage = () => {
+  return (
+    <div>
+      <img src='./logo.png'></img>
+      <h3>All ebooks in our website</h3>
+    </div>
+  )
+}
+
+export default MainPage;
